aec4e6465e92ea9acc2bd579dc011d28681d163d
Steve Cordova <cordovas@bc.edu>
2018-06-26 15:22:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-Write a speak_to_grandma method.
+# Write a speak_to_grandma method.
 def speak_to_grandma(x)
   if x == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
@@ -15,7 +15,7 @@ end
 #     return "HUH?! SPEAK UP, SONNY!"
 #   elsif phrase == "I LOVE YOU GRANDMA!"
 #     return "I LOVE YOU TOO PUMPKIN!"
-# 
+#
 #   else
 #     return "NO, NOT SINCE 1938!"
 #   end
