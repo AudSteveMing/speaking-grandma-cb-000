92674c3bf976c1e7054b8e252ea3f1b1152eaa19
Steve Cordova <cordovas@bc.edu>
2018-06-26 15:20:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I lOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
 
   else
     return "NO, NOT SINCE 1938!"
